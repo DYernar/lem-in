34a5c0ee3af2a65c67c06fc543a4e771d8143ab8
DYernar <eduisebai.1@gmail.com>
2020-03-08 04:11:53 +0600

    sd

--- Solution.go
+++ Solution.go
@@ -3,14 +3,20 @@ package main
 import(
 	"os"
 	"bufio"
-
+	"strconv"
 )
 
 func Solution(ph []*Room) {
 	fileOutput, _ :=os.Create("output.txt")
 	writer := bufio.NewWriter(fileOutput)
-	for _, p := range ph {
-		writer.WriteString("L1-"+p.Name+"\n")
+		for _, p := range ph {
+			for i := 0; i < GlobalData.AntNum; i++ {
+
+			writer.WriteString(" L"+strconv.Itoa(i)+"-"+p.Name)
+		}
+		writer.WriteString("\n")
 		writer.Flush() 
+
 	}
+
 }
